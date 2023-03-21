c92a306b46a0c95576f52d1a960e05b9c574ae8a
Tim1222 <timofeu47@gmail.com>
2023-03-21 07:05:26 +0200

    working with components

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,20 @@ import React from 'react';
 import './App.css';
 import Technologies from './Technologies';
 import Header from './Header';
+import Star from "./Star";
 
 const App = () => {
     return (
         <div>
+            <AppTitle/>
+            <Star/>
             <Header/>
             <Technologies/>
         </div>
     );
 }
 
-
-
-
-
-
-
+function AppTitle () {
+    return <>Hello everybody</>
+}
 export default App;
---
+++ src/Star.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Stars from "./Stars";
+
+
+function Star() {
+    return (
+        <div>
+            <Stars />
+            <Stars />
+            <Stars />
+            <Stars />
+            <Stars />
+        </div>
+    );
+
+}
+
+export default Star;
