afdbd5825f62535d4e2c7ecb5652facba7f75844
Tim1222 <timofeu47@gmail.com>
2023-03-22 08:29:51 +0200

    traning props

---
+++ src/components/Accordion/AccordionTitle.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+function AccordionTitle () {
+    console.log('AccordionTitle rendering')
+    return  <h3>Меню</h3>
+}
+
+export default AccordionTitle;
---
+++ src/components/Rating/Rating.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Star from "./Star";
+
+
+function Rating () {
+    return (
+        <div>
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+            <Star />
+        </div>
+    );
+
+}
+
+export default Rating;
