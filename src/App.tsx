8b50a4883420a2e05ff1428d4f5ccd63c6be306d
Tim1222 <timofeu47@gmail.com>
2023-03-29 21:02:56 +0300

    add footer

--- src/App.tsx
+++ src/App.tsx
@@ -2,47 +2,18 @@ import React from 'react';
 import './App.css';
 import Technologies from './Technologies';
 import Header from './Header';
-import Star from "./Star";
-import {log} from "util";
+import Footer from "./Footer";
+
 
 const App = () => {
     return (
         <div>
-            <AppTitle/>
-            <Star/>
             <Header/>
             <Technologies/>
-            <Accordion/>
+            <Footer/>
         </div>
     );
 }
 
-function Accordion () {
-    return <div>
-        <AccordionTitle/>
-        <AccordionBody/>
 
-    </div>
-}
-
-function AccordionTitle () {
-    console.log('AccordionTitle rendering')
-    return  <h3>Меню</h3>
-}
-
-function AccordionBody () {
-    console.log('AccordionBody rendering')
-    return (
-
-        <ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
-    )
-}
-
-function AppTitle () {
-    return <>Hello everybody</>
-}
 export default App;
