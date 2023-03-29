a0e23bf987a386edd8799e3e88b259ccc62c56cb
Tim1222 <timofeu47@gmail.com>
2023-03-29 22:38:57 +0300

    create folder components

--- src/components/Accordion/AccordionTitle.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-
-function AccordionTitle(props: any) {
-    // console.log('AccordionTitle rendering')
-    return  <h3>{props.title}</h3>
-}
-
-export default AccordionTitle;
---
+++ src/components/Profile.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://cdn.pixabay.com/photo/2020/08/28/11/10/ocean-5524264_1280.jpg'/>
+        </div>
+        <div>
+            <img
+                src='https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg'/>
+            ava+description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                <div>
+                    post 1
+                </div>
+                <div>
+                    post 2
+                </div>
+            </div>
+        </div>
+    </div>
+}
+
+export default Profile;
