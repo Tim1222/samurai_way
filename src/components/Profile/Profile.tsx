3995f37ba4edafaa80c3dd664a254fcacb19a088
Tim1222 <timofeu47@gmail.com>
2023-04-05 10:05:20 +0300

    add route for pages

--- src/components/Profile/Profile.tsx
+++ src/components/Profile/Profile.tsx
@@ -3,13 +3,11 @@ import s from './Profile.module.css'
 import MyPosts from "./MyPosts/MyPosts";
 
 const Profile = () => {
-    return <div className={s.content}>
+    return <div>
         <div>
             <img src='https://cdn.pixabay.com/photo/2020/08/28/11/10/ocean-5524264_1280.jpg'/>
         </div>
         <div>
-            <img
-                src='https://images.pexels.com/photos/45201/kitty-cat-kitten-pet-45201.jpeg?cs=srgb&dl=pexels-pixabay-45201.jpg&fm=jpg'/>
             ava+description
         </div>
         <MyPosts/>
